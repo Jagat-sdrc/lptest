d89b3813e6e2db9b9b742ed6ab7c3370bbd83369
ratikanta-sdrc <ratikanta@sdrc.co.in>
2018-02-15 20:25:25 +0530

    bug#2087: delete patient work

--- src/providers/registered-patient-service/registered-patient-service.ts
+++ src/providers/registered-patient-service/registered-patient-service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { ConstantProvider } from '../constant/constant';
 
 /**
  * This service will only provide service to Registered Patient component
@@ -10,8 +11,29 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RegisteredPatientServiceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello RegisteredPatientServiceProvider Provider');
-  }
+  constructor(private storage: Storage){}
 
+  /**
+   * 
+   * @param babyCode The baby code of the patient to which we are going to delete
+   */
+  deletePatient(babyCode: string): Promise<any>{
+    let promise: Promise<any> = new Promise((resolve, reject)=>{
+      this.storage.get(ConstantProvider.dbKeyNames.patient)
+      .then(data=>{
+        data = (data as IPatient[]).splice((data as IPatient[]).findIndex(d=> d.babyCode === babyCode), 1)
+        this.storage.set(ConstantProvider.dbKeyNames.patient, data)
+        .then(result=>{
+          resolve()
+        })
+        .catch(err=>{
+          reject(err.message)  
+        })
+      })
+      .catch(err=>{
+        reject(err.message)
+      })
+    })
+    return promise;
+  }
 }
